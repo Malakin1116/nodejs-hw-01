5f015515b3482a8ea4619ac6bb72681f8c76ed12
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-12-02 02:11:16 +0200

    2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,31 @@
-const generateContacts = async (number) => {};
+// import * as fs from 'node:fs';
+// // import { createFakeContact } from '../utils/createFakeContact.js';
+// import { PATH_DB_CONTACTS } from '../constants/contacts.js';
+
+// const generateContacts = async (number) => {
+//   //   const contact = createFakeContact();
+//   //   console.log(contact);
+
+//   //   const contactList = Array(number).fill(0).map(createFakeContact);
+
+//   //   const buffer = await fs.readFile(PATH_DB_CONTACTS, (error, data) => {
+//   //     console.log(error);
+//   //     console.log(data);
+//   //   });
+
+//   const data = await fs.readFile(PATH_DB_CONTACTS, 'utf-8');
+//   console.log(JSON.parse(data)[0]);
+// };
+
+// generateContacts(5);
+
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const contactList = await readContacts();
+  const newContact = Array(number).fill(0).map(createFakeContact);
+  const newList = [...contactList, ...newContact];
+};
 
 generateContacts(5);
